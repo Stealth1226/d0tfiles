a51c5861ca00f43f80f3c7fc20fc9d13d7187b52
Stealth1226 <rishawniyer06@gmail.com>
2022-03-24 10:42:12 +0530

    add installed packages list
    for easy migration

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+# Configs
 for i in $(ls config); do
     rm -rf config/$i
     cp -r ~/.config/$i config/
 done
+
+# Installed packages
+pacman -Qe | awk '{ print $1 }' | tee packages.txt >/dev/null
